f0d05194d0edbd063679e37be28818db05762660
avinash36o <avinashprasadsinha996@gmail.com>
2026-05-06 23:21:37 +0530

    add hero and trust sections

--- src/landing_page/home/Pricing.js
+++ src/landing_page/home/Pricing.js
@@ -1,7 +1,12 @@
+import "./Pricing.css";
+
 export default function Pricing() {
   return (
-    <>
-      <h3>Pricing</h3>
-    </>
+    <div className="pricing">
+      <h1>Transparent Pricing</h1>
+      <p style={{ marginTop: "50px" }}>
+        Choose the plan that fits your trading frequency and strategy.
+      </p>
+    </div>
   );
 }
